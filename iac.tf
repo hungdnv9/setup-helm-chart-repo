de1be0c9cb680c3c83d176b7ed80d84653b01678
hungdnv9 <hungdnv9@gmail.com>
2022-05-18 15:05:45 +0700

    fixci

--- iac.tf
+++ iac.tf
@@ -30,3 +30,48 @@ resource "aws_s3_object" "charts" {
   bucket = aws_s3_bucket.helm_repo.id
   key    = "charts/"
 }
+
+// Github Action 
+// Required: Created an IAM user, called: github-action-helm-chart-repo
+
+resource "aws_iam_policy" "github_action" {
+  name        = "github-action-helm-chart-repo-policy"
+  path        = "/"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode(
+    {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Sid": "files",
+                "Effect": "Allow",
+                "Action": [
+                    "s3:PutObjectAcl",
+                    "s3:PutObject",
+                    "s3:GetObjectAcl",
+                    "s3:GetObject",
+                    "s3:DeleteObject"
+                ],
+                "Resource": [
+                    "arn:aws:s3:::${aws_s3_bucket.helm_repo.id}/charts/*",
+                    "arn:aws:s3:::${aws_s3_bucket.helm_repo.id}/charts"
+                ]
+            },
+            {
+                "Sid": "bucket",
+                "Effect": "Allow",
+                "Action": "s3:ListBucket",
+                "Resource": "arn:aws:s3:::${aws_s3_bucket.helm_repo.id}"
+            }
+        ]
+    }
+  )
+}
+
+resource "aws_iam_policy_attachment" "github_action" {
+  name       = "github-action-helm-chart-repo-policy"
+  users      = ["github-action-helm-chart-repo"]
+  policy_arn = aws_iam_policy.github_action.arn
+}
